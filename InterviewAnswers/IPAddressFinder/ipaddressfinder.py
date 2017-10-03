031c681bec65d01ff48cf592e2d236fa38e2a01d
Anusha <anushabalu89@gmail.com>
2017-10-03 00:46:48 -0400

    changed few files

--- InterviewAnswers/IPAddressFinder/ipaddressfinder.py
+++ InterviewAnswers/IPAddressFinder/ipaddressfinder.py
@@ -5,32 +5,39 @@ import netaddr
 import socket
 
 ip_before_sorting = []
-if sys.argv[1:]:
-   print "File: %s" % (sys.argv[1])
-   logfile = sys.argv[1]
-else:
-   logfile = raw_input("Please enter the file directories to parse, root/devops/ ")
-for dirpath, dirnames, filenames in os.walk(logfile):
-   for files in filenames:
-      ips = []
-      totalPath = dirpath+"/"+files
-      #print (totalPath)
-      file = open(totalPath, "r")
-      for text in file.readlines():
-         text = text.rstrip()
-         count = 0
-         regex = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})',text)
-	 if regex is not None:
-            for match in regex:
-               if netaddr.valid_ipv4(match):
-                  ips.append(match)
+try:
+   if sys.argv[1:]:
+      print "File: %s" % (sys.argv[1])
+      logfile = sys.argv[1]
+   else:
+      logfile = raw_input("Please enter the file directories to parse,eg: root/devops: ")
+   for dirpath, dirnames, filenames in os.walk(logfile):
+      for files in filenames:
+         ips = []
+         totalPath = dirpath+"/"+files
+         try:
+            file = open(totalPath, "r")
+            for text in file.readlines():
+               text = text.rstrip()
+               count = 0
+               regex = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})',text)
+	       if regex is not None:
+                  for match in regex:
+                     if netaddr.valid_ipv4(match):
+                        ips.append(match)
       
-      seen = set()
-      for ip in ips:
-          key = tuple(ip.split(".")[:2])
-          if key not in seen:
-             ip_before_sorting.append(ip)
-             seen.add(key)
-      file.close()
-finalIp = sorted(ip_before_sorting, key=lambda x:map(int, x.split('.')))
-print (finalIp)
+            seen = set()
+            for ip in ips:
+               key = tuple(ip.split(".")[:2])
+               if key not in seen:
+                  ip_before_sorting.append(ip)
+                  seen.add(key)
+         finally:
+            file.close()
+
+   finalIp = sorted(ip_before_sorting, key=lambda x:map(int, x.split('.')))
+   for result in finalIp:
+      print(result)
+
+except IOError, (errno, strerror):
+        print "I/O Error(%s) : %s" % (errno, strerror)
--- InterviewAnswers/LogAnalyzer/LogAnalyze.py
+++ InterviewAnswers/LogAnalyzer/LogAnalyze.py
@@ -3,31 +3,39 @@ import re
 import os
 import operator
 
-if sys.argv[1:]:
-   print "File: %s" % (sys.argv[1])
-   logfile = sys.argv[1]
-else:
-   logfile = raw_input("Please enter a log files directory to parse, e.g /root/devops/ ")
-mapVal = {}
-for dirpath, dirnames, filenames in os.walk(logfile):
-   for files in filenames:
-      if files.endswith(".log"):
-         totalPath = dirpath+"/"+files
-         file = open(totalPath, "r")
-         for text in file.readlines():
-            text = text.strip()
-            if 'ERROR' in text:
-               occ = [m.start() for m in re.finditer('[^|]*',text)]
-	       part1 = text[occ[4]:occ[5]]
-	       key = part1.strip()
-	       if key in mapVal:
-	          mapVal[key] += 1
-		  updateVal = {key: mapVal[key]}
-                  mapVal.update(updateVal)
-               else:
-		  count = 1
-		  newVal = {key: count}
-                  mapVal.update(newVal)
+try:
+   if sys.argv[1:]:
+      print "File: %s" % (sys.argv[1])
+      logfile = sys.argv[1]
+   else:
+      logfile = raw_input("Please enter a log files directory to parse, e.g /root/devops: ")
+   mapVal = {}
+   for dirpath, dirnames, filenames in os.walk(logfile):
+      for files in filenames:
+         if files.endswith(".log"):
+            totalPath = dirpath+"/"+files
+            try:
+               file = open(totalPath, "r")
+               for text in file.readlines():
+                  text = text.strip()
+                  if 'ERROR' in text:
+                      occ = [m.start() for m in re.finditer('[^|]*',text)]
+	              part1 = text[occ[4]:occ[5]]
+	              key = part1.strip()
+	              if key in mapVal:
+	                 mapVal[key] += 1
+		         updateVal = {key: mapVal[key]}
+                         mapVal.update(updateVal)
+                      else:
+		         count = 1
+		         newVal = {key: count}
+                         mapVal.update(newVal)
+            finally:
+               file.close()
 
-sortedMap = sorted(sorted(mapVal.iteritems()), key=operator.itemgetter(1), reverse=True)
-print (sortedMap)
+   sortedMap = sorted(sorted(mapVal.iteritems()), key=operator.itemgetter(1), reverse=True)
+   for result in sortedMap:
+      print(result)
+
+except IOError, (errno, strerror):
+        print "I/O Error(%s) : %s" % (errno, strerror)
